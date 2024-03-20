573c5995d336060a08cac75a6ba74e37cbc5361c
ibui2628 <92432744+ibui2628@users.noreply.github.com>
2024-03-20 10:26:46 +0900

    Added Open in Map feature and error dialog

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const maininfo = document.querySelector('.main-info');
 var id,power = false;
 
-on_off_btn.addEventListener("click",()=>{
+on_off_btn.addEventListener("click",onoff);
+function onoff(){
     if (power) {
         navigator.geolocation.clearWatch(id);
         on_off_btn.style = '';
@@ -9,25 +10,34 @@ on_off_btn.addEventListener("click",()=>{
         maininfo.style = '';
         power = false;
     } else {
-        id = navigator.geolocation.watchPosition(gps, error, { "enableHighAccuracy": true });
+        id = navigator.geolocation.watchPosition(gps, err, { "enableHighAccuracy": true });
         on_off_btn.style.background = '#1e56a0';
         on_off_btn.innerText = 'ON';
         maininfo.style.borderColor = '#080';
         power = true;
     }
-});
-
+}
 function gps(p) {
-    speed_h.innerText = Math.round(p.coords.speed * 3.6);
-    speed_m.innerText = Math.round(p.coords.speed);
+    const co = p.coords;
+    speed_h.innerText = Math.round(co.speed * 3.6);
+    speed_m.innerText = Math.round(co.speed);
 
-    latitude.innerHTML = p.coords.latitude;
-    longitude.innerHTML = p.coords.longitude;
-    accuracy.innerHTML = Math.round(p.coords.accuracy);
-    altitude.innerHTML = Math.round(p.coords.altitude);
-    heading.innerHTML = Math.round(p.coords.heading);
+    latitude.innerHTML = co.latitude;
+    longitude.innerHTML = co.longitude;
+    accuracy.innerHTML = Math.round(co.accuracy);
+    altitude.innerHTML = Math.round(co.altitude);
+    heading.innerHTML = Math.round(co.heading);
+
+    mapurl.setAttribute("href", 'https://www.google.com/maps/search/?api=1&query=' + co.latitude + "," + co.longitude);
 }
-function error(p) {
+function err(p) {
+    err_dia.showModal();
+    errcode.innerText = p.code;
+}
+errclose.addEventListener("click",()=>{
+    err_dia.close();
+});
+err_dia.addEventListener("close",()=>{
+    onoff();
     maininfo.style.borderColor = '#a11';
-    alert("ERROR!!\n code:"+p.code);
-}
+});
