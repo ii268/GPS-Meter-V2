2fc1f7236189a58a93a5d4b5878a375276b661a3
ibui2628 <92432744+ibui2628@users.noreply.github.com>
2024-03-22 13:12:02 +0900

    Add dark mode

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 const key_name = 'GPS-MATER-V1';
 var seting_data;
+
+const maininfo = document.querySelector('.main-info');
+var id, power = false;
+
+const bodys = document.querySelector('body');
+function darkmode(e) {
+    if (e) {
+        bodys.classList.add('dark');
+    } else {
+        bodys.classList.remove('dark');
+    }
+}
+
 if (!localStorage.hasOwnProperty(key_name)) {
     console.info('create key');
     seting_data = '{ "darkmode": false, "speedgoh": 0, "speedgos": 0}';
     localStorage.setItem(key_name, seting_data);
     seting_data = JSON.parse(seting_data);
+    info_dia.showModal();
 } else {
     seting_data = JSON.parse(localStorage.getItem(key_name));
     speedgoh.value = seting_data["speedgoh"];
     speedgos.value = seting_data["speedgos"];
-}
 
-const maininfo = document.querySelector('.main-info');
-var id, power = false;
+    darkmode(seting_data["darkmode"]);
+    dackmode_set.checked = seting_data["darkmode"];
+}
 
 on_off_btn.addEventListener("click", onoff);
 
@@ -31,6 +45,7 @@ function onoff() {
         power = true;
     }
 }
+
 function gps(p) {
     const co = p.coords;
     speed_h.innerText = Math.round(co.speed * 3.6) + eval(speedgoh.value);
@@ -44,6 +59,7 @@ function gps(p) {
 
     mapurl.setAttribute("href", 'https://www.google.com/maps/search/?api=1&query=' + co.latitude + "," + co.longitude);
 }
+
 function err(p) {
     err_dia.showModal();
     errcode.innerText = p.code;
@@ -59,13 +75,24 @@ err_dia.addEventListener("close", () => {
 seting_btn.addEventListener("click", () => {
     setting_dia.showModal();
 });
+
 settingclose.addEventListener("click", () => {
     setting_dia.close();
 });
+
 setting_dia.addEventListener("close", () => {
     seting_data["speedgoh"] = speedgoh.value;
     seting_data["speedgos"] = speedgos.value;
 
+    darkmode(dackmode_set.checked);
+    seting_data["darkmode"] = dackmode_set.checked;
+
     localStorage.setItem(key_name, JSON.stringify(seting_data));
     console.log(seting_data);
+});
+info_btn.addEventListener("click",()=>{
+    info_dia.showModal();
+});
+infoclose.addEventListener("click",()=>{
+    info_dia.close();
 });
